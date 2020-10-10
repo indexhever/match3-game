5023cf3c2677970704eae092ebfff4b1ccb7d64d
Heverton Sarah <heverton.sarah@gmail.com>
2020-10-10 16:23:22 -0300

    Generate Grid generation

---
+++ Assets/Scripts/GridFramework/ItemFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GridFramework
+{
+    public interface ItemFactory
+    {
+        Vector2 MeasuresInUnit { get; }
+
+        Item Create(Vector2 newItemPosition);
+    }
+}
---
+++ Assets/Scripts/Tests/GridGenerationTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using GridFramework;
+
+namespace Tests.Unit.Grid
+{
+    public class GridGenerationTests
+    {
+        [Test]
+        public void GridHasRowsAndColumns()
+        {
+            int rows = 3;
+            int columns = 2;
+            ItemFactory itemFactory = CreateItemFactory(new Vector2(1, 2));
+            Vector2 origin = Vector2.zero;
+            GameGrid grid = CreateGameGrid(rows, columns, itemFactory, origin, 1);
+
+            grid.GenerateItems();
+
+            Assert.AreEqual(rows, grid.Rows);
+            Assert.AreEqual(columns, grid.Columns);
+        }
+
+        [Test]
+        public void GetFirstItemByRowAndColumn()
+        {
+            int rows = 3;
+            int columns = 3;
+            ItemFactory itemFactory = CreateItemFactory(new Vector2(1, 2));
+            Vector2 origin = Vector2.zero;
+            GameGrid grid = CreateGameGrid(rows, columns, itemFactory, origin, 1);
+            grid.GenerateItems();
+
+            Item item = grid.GetItemByRowColumn(0, 0);
+
+            Assert.IsNotNull(item);
+        }
+
+        private ItemFactory CreateItemFactory(Vector2 itemMeasuresInUnit)
+        {
+            return new MockItemFactory(itemMeasuresInUnit);
+        }
+
+        private GameGrid CreateGameGrid(int rows, int columns, ItemFactory itemFactory, Vector2 origin, float offsetBetweenItems)
+        {
+            return new GameGrid(rows, columns, itemFactory, origin, offsetBetweenItems);
+        }
+    }
+}
