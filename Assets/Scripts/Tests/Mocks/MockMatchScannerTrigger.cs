016a40d16fda8ba91be2172226e51215deb7c784
Heverton Sarah <heverton.sarah@gmail.com>
2020-10-12 20:00:03 -0300

    Add match scanner trigger calling test

--- Assets/Scripts/Tests/Mocks/MockMatchScannerTrigger.cs
+++ Assets/Scripts/Tests/Mocks/MockMatchScannerTrigger.cs
@@ -6,9 +6,11 @@ namespace Tests
 {
     public class MockMatchScannerTrigger : MatchScannerTrigger
     {
+        public bool WasCalled { get; internal set; }
+
         public void Scan()
         {
-            
+            WasCalled = true;
         }
     }
 }
